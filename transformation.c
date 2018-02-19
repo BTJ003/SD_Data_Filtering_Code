13882120632fbd98dfe64876096afaad0c041be9
BTJ003 <35822080+BTJ003@users.noreply.github.com>
2018-02-19 17:05:44 -0600

    Update transformation.c
    Removed commented out old information from previous code batch

--- transformation.c
+++ transformation.c
@@ -1,32 +1,5 @@
 #include "transformation.h"
 
-/*
-
-float Data_to_AI_Transform(float DriverData.Lng,float DriverData.Lat,float DriverData.Heading,float AIData.Lng,float AIData.Lat,float AIData.Heading) 
-{
-    if(25 > DriverData.Lng)
-        return 0; // unsure how to make request for new data 
-    if(DriverData.Lng > 50)
-        return 0; // unsure how to make request for new data  
-    if(-125 > DriverData.Lat)
-        return 0; // unsure how to make request for new data 
-    if(DriverData.Lat > -65)
-        return 0; // unsure how to make request for new data  
-    
-    
-    //When I know both forms I can create equations to properly convert these
-    AIData.Lng = DriverData.Lng;
-    AIData.Lat = DriverData.Lat;
-    AIData.Heading = DriverData.Heading;
-    
-    //Need to determine how the rudder works and determine proper transformation for this data
-    DriverRudder.RudderDegree = AIRudder.RudderDegree;
-    
-    return 0;
- }
-  
-*/
-
 AIData transformDataToAI(DriverData in){
 	AIData out;
 	//Check for validity
