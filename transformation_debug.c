4730fb9463aeec321c38dc2163ab9c9cd35d40fd
Kaleb Crow <kthedude95@gmail.com>
2018-02-23 14:33:29 -0600

    Debugged the debug code for lat and lng transformations.

--- transformation_debug.c
+++ transformation_debug.c
@@ -4,29 +4,32 @@
 #include "transformation.h"
 
 float INCREMENT = 5.0;
+float LAT_VALID = -80.0;
+float LNG_VALID = 35;
 
 int main(){
-	for(float i = 15; i <= 60, i+=INCREMENT){ //For Longitude
+	for(float i = 15; i <= 60; i+=INCREMENT){ //For Longitude
 		DriverData input;
 		input.lng = i;
+		input.lat = LAT_VALID;
 		AIData test = transformDataToAI(input);
 		if(test.valid){
-			printf("The input Longitude was %f and the transformed value is: %i\n", i, test.rudderDegree);
+			printf("The input Longitude was %f and the transformed value is: %f\n", i, test.lng);
 		} else{
 			printf("The input Longitude was %f and is not valid\n", i);
 		}
 	}
-	for(float i = -135; i <= -55, i+=INCREMENT){ //For Latitude
+	for(float i = -135; i <= -55; i+=INCREMENT){ //For Latitude
 		DriverData input;
 		input.lat = i;
+		input.lng = LNG_VALID;
 		AIData test = transformDataToAI(input);
 		if(test.valid){
-			printf("The input Latitude was %f and the transformed value is: %i\n", i, test.rudderDegree);
+			printf("The input Latitude was %f and the transformed value is: %f\n", i, test.lat);
 		} else{
 			printf("The input Latitude was %f and is not valid\n", i);
 		}
 	}	
-	}
 	for(float i = -110.0; i <= 110.0; i+=INCREMENT){ //For Rudder
 		AIRudder input;
 		input.rudderDegree = i;
