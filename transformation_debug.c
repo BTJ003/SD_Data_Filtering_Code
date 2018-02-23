013e0fa6ba188a7cf3fd98da38fe099421424ae4
BTJ003 <35822080+BTJ003@users.noreply.github.com>
2018-02-23 13:50:22 -0600

    Update transformation_debug.c
    Created additional setup for longitude and latitude testing (awaiting actually transformation to be tested)

--- transformation_debug.c
+++ transformation_debug.c
@@ -3,17 +3,38 @@
 
 #include "transformation.h"
 
-float INCREMENT = 31.4159;
+float INCREMENT = 5.0;
 
 int main(){
-	for(float i = -110.0; i <= 110.0; i+=INCREMENT){
+	for(float i = 15; i <= 60, i+=INCREMENT){ //For Longitude
+		DriverData input;
+		input.lng = i;
+		AIData test = transformDataToAI(input);
+		if(test.valid){
+			printf("The input Longitude was %f and the transformed value is: %i\n", i, test.rudderDegree);
+		} else{
+			printf("The input Longitude was %f and is not valid\n", i);
+		}
+	}
+	for(float i = -135; i <= -55, i+=INCREMENT){ //For Latitude
+		DriverData input;
+		input.lat = i;
+		AIData test = transformDataToAI(input);
+		if(test.valid){
+			printf("The input Latitude was %f and the transformed value is: %i\n", i, test.rudderDegree);
+		} else{
+			printf("The input Latitude was %f and is not valid\n", i);
+		}
+	}	
+	}
+	for(float i = -110.0; i <= 110.0; i+=INCREMENT){ //For Rudder
 		AIRudder input;
 		input.rudderDegree = i;
 		DriverRudder test = transformRudderToDriver(input);
 		if(test.valid){
-			printf("The input was %f and the transformed value is: %i\n", i, test.rudderDegree);
+			printf("The input Rudder Ratio was %f and the transformed value is: %i\n", i, test.rudderDegree);
 		} else{
-			printf("The input was %f and is not valid\n", i);
+			printf("The input Rudder Ratio was %f and is not valid\n", i);
 		}
 	}
 }
